8687495565fae1055cda3d151c66e766845855cd
Emmaloto <icyblaze16@gmail.com>
2018-06-22 21:10:53 -0500

    Added multiple robots

--- Robo-Click/js/robot.js
+++ Robo-Click/js/robot.js
@@ -11,9 +11,12 @@ var h = w;
 var canvasW = 900;
 var canvasH = 600;
 
-var timeCounter = 0;
-var stopGame = false;
+//var timeCounter = 0;
+//var stopGame = false;
+var gameRunning = true;
 var canvas;
+
+var robots = [];
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -28,6 +31,11 @@ function setup() {
   centerCanvas();
   line(1, 1, 0, canvasW);
   line(1, 1, canvasH, 0);
+  
+  var robot = new Robot(20, 20, 100, 100, color(219, 0, 0));
+  robots.push(robot);
+  
+  //background(0, 67, 90);
 }
 
 
@@ -40,143 +48,201 @@ var drawShape = function(nx, ny, nw, nh){
 };
 
 // Function for robot
-var drawRobot = function(nx, ny, nw, nh){
+var Robot = function(nx, ny, nw, nh, color){
 
-    var l = (nw + nh)/4 ;
 
-	var x = nx + w/2;
-    var y = ny + h/2;
-	
+    this.x = nx;
+    this.y = ny;
+    
+    
+    this.w = nw;
+    this.h = nh;
+    this.l = 0;
+    
+    this.dx = 0;
+    this.dy = 0;
+    
+    this.isMoving = true;
+    this.timeCounter = 0;
+    
+    this.color = color;
+
+};
+
+Robot.prototype.draw = function() {
+
+    // Bounding rectangle
+    //fill(53, 04, 130);
+    //rect(this.x , this.y, this.w, this.h);
+    
+    
+    var cx = this.x + this.w/2;
+    var cy = this.y + this.h/2;
+    this.l = (this.w + this.h)/4 ;
+
+    
     //Arms
-    stroke(219, 0, 0);
-    strokeWeight(l/6);
-    line(x, y-l/3, x-l*0.9, y);
-    line(x, y-l/3, x+l*0.9, y);
-    line(x+l*0.9, y, x+l*0.9, y-l/3);
-    line(x-l*0.9, y, x-l*0.9, y-l/3);
+    stroke(this.color);
+    strokeWeight(this.l/6);
+    line(cx, cy-this.l/3, cx-this.l*0.9, cy);
+    line(cx, cy-this.l/3, cx+this.l*0.9, cy);
+    line(cx+this.l*0.9, cy, cx+this.l*0.9, cy-this.l/3);
+    line(cx-this.l*0.9, cy, cx-this.l*0.9, cy-this.l/3);
     
     noStroke();
-    fill(219, 0, 0);
-    arc(x+l*0.9,y-l*(2/5),l/3,l/4,-45,225);
-    arc(x-l*0.9,y-l*(2/5),l/3,l/4,-45,225);
+    fill(this.color);
+    arc(cx+this.l*0.9,cy-this.l*(2/5),this.l/3,this.l/4,-45,225);
+    arc(cx-this.l*0.9,cy-this.l*(2/5),this.l/3,this.l/4,-45,225);
     
     //Head
-    rect(x - l/3, y-(11*l)/10, (2/3)*l, l/2, 8);
-    ellipse(x,y-l,l/2,l/2);
-    rect(x-l/10, y-(6*l)/10, l/5, l/10);
+    rect(cx - this.l/3, cy-(11*this.l)/10, (2/3)*this.l, this.l/2, 8);
+    ellipse(cx,cy-this.l,this.l/2,this.l/2);
+    rect(cx-this.l/10, cy-(6*this.l)/10, this.l/5, this.l/10);
     fill(201, 201, 201);
-    rect(x-l/6, y-(9/12)*l, l/3, l/12, 10);
+    rect(cx-this.l/6, cy-(9/12)*this.l, this.l/3, this.l/12, 10);
     
     //Body
-    fill(219, 0, 0);
-    rect(x-l/2, y-l/2, l, l, 8);
+    fill(this.color);
+    rect(cx-this.l/2, cy-this.l/2, this.l, this.l, 8);
     fill(255, 248, 51);
-    rect(x-l*(2/6), y-l/3, l/3, l/10, 4);
+    rect(cx-this.l*(2/6), cy-this.l/3, this.l/3, this.l/10, 4);
     fill(255, 157, 0);
-    rect(x-l*(2/6), y-l/6, l*(2/3), l/20, 4);
+    rect(cx-this.l*(2/6), cy-this.l/6, this.l*(2/3), this.l/20, 4);
     
     fill(209, 209, 209);
-    ellipse(x-l/6,y-(4*l)/10, l/20, l/20);
-    ellipse(x+l/6,y-(4*l)/10,l/20,l/20);
+    ellipse(cx-this.l/6,cy-(4*this.l)/10, this.l/20, this.l/20);
+    ellipse(cx+this.l/6,cy-(4*this.l)/10,this.l/20,this.l/20);
     
     //Legs
-    fill(219, 0, 0);
-    rect(x-l/3, y+l/2, l/5, l*0.6);
-    rect(x+l/3-l/5, y+l/2, l/5, l*0.6);
-    rect(x-l/2, y+l, l/3, l/10, 4);
-    rect(x+l/3-l/5, y+l, l/3, l/10, 4);
+    fill(this.color);
+    rect(cx-this.l/3, cy+this.l/2, this.l/5, this.l*0.6);
+    rect(cx+this.l/3-this.l/5, cy+this.l/2, this.l/5, this.l*0.6);
+    rect(cx-this.l/2, cy+this.l, this.l/3, this.l/10, 4);
+    rect(cx+this.l/3-this.l/5, cy+this.l, this.l/3, this.l/10, 4);
     
     //Left eye
     fill(255, 255, 255);
-    ellipse(x-l/6,y-(9*l)/10, l/10, l/10);
+    ellipse(cx-this.l/6,cy-(9*this.l)/10, this.l/10, this.l/10);
     fill(255, 166, 0);
-    ellipse(x-l/6,y-(9*l)/10, (3/40)*l, (3/40)*l);
+    ellipse(cx-this.l/6,cy-(9*this.l)/10, (3/40)*this.l, (3/40)*this.l);
     
     //Right eye
     fill(255, 255, 255);
-    ellipse(x+l/6,y-(9*l)/10,l/10,l/10);
+    ellipse(cx+this.l/6,cy-(9*this.l)/10,this.l/10,this.l/10);
     fill(255, 166, 0);
-    ellipse(x+l/6,y-(9*l)/10, (3/40)*l, (3/40)*l);
-};
-
-var debugText = function(){
-    fill(0, 34, 255);
-    text("x: " + x, 100, 100);
-    text("y: " + y, 100, 120);
-    text("dx: " + dx, 100, 140);
-    text("dy: " + dy, 100, 160);
+    ellipse(cx+this.l/6,cy-(9*this.l)/10, (3/40)*this.l, (3/40)*this.l);
     
-    //text(x < 0 && x + dx + w/2 > 400, 200, 200);
+    this.animate();
     
-    text("Counter: " + timeCounter, 100, 200);
-    text(stopGame, 100, 220);
-    text(!stopGame, 100, 240);
 };
 
-var restart = function() {
-    fill(144, 214, 90);
-    rect(100,100,100,100);
-    if(true){
-        draw();
-        
-    }
+Robot.prototype.animate = function() {
 
-};
-
-
-draw = function() {
-   
-   background(0, 67, 90);
-   fill(78, 64, 230);
-   rect(4, 4, canvasW-4, canvasH-4);
-   
-   
-   if(!stopGame){
-     
+   if(this.isMoving){
+       
 	 // Changes speed after a certain time
-     if(timeCounter % 100 === 0){
-        dx = round(random(-10, 10));
-        dy = round(random(-10, 10));
+        if(this.timeCounter % 100 === 0){
+            this.dx = round(random(-5, 15));
+            this.dy = round(random(-5, 15));
         
         // Makes sure ball does not slow down
-        if(dx >= -1 && dx <= 1){
-            dx = dx + 4;
+            if(this.dx >= -1 && this.dx <= 1){
+                this.dx = this.dx + 4;
+            }
+            if(this.dy >= -1 && this.dy <= 1){
+                this.dy = this.dy + 4;
+            }
         }
-        if(dy >= -1 && dy <= 1){
-            dy = dy + 4;
-        }
-     }
    
      // Prevents object from going outside the window
-     if(x < 0 || x + dx + w > canvasW){
-        dx = -dx;
-     }
+        if(this.x < 0 || this.x + this.dx + this.w > canvasW){
+            this.dx = -this.dx;
+        }
         
-     if(y < 0 || y + dy + h > canvasH){
-        dy = -dy;
-     }
+        if(this.y < 0 || this.y + this.dy + this.h > canvasH){
+            this.dy = -this.dy;
+        }
     
+    /*
      x = x + dx;
      y = y + dy;
+    */
+        this.move(this.dx, this.dy);
+        this.timeCounter ++;
     
+        // Ensures timeCounter does not get too big
+        if(this.timeCounter > 1000){ this.timeCounter = 0; }
     
+    }else{ // You won, so stop the game
+    }
+};
+
+Robot.prototype.mouseAction = function() {
     
-     // If you catch the box
-     if(mouseIsPressed && mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h){
-        dx = 0; 
-        dy = 0;
-        stopGame = true;
-     }
+    var buttonHover = mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h;
     
-     timeCounter ++;
+    /*
+    // For highlighting buttons when passed over
+    // Within button + button not highlighted -> highlight button
+    if(buttonHover && !this.buttonStates[0]){
+      this.buttonStates[0] = true;
+      
+      var col = changeColor(0, 0, 0, -50, this.inColor);
+      this.inColor = col;
+      
+    // NOT Within button + button highlighted -> UN-highlight button  
+    }else if (!buttonHover && this.buttonStates[0]){
+      this.buttonStates[0] = false;  
+      
+      var col = changeColor(0, 0, 0, 50, this.inColor);
+      this.inColor = col;
+    }
+    */
     
-    // Ensures timeCounter does not get too big
-    if(timeCounter > 1000){ timeCounter = 0; }
+    // If button is pressed once, start playing sound
+    // Mouse Click + Within robot + robot not stopped yet -> stop robot
+    if(mouseIsPressed && buttonHover){
+        this.dx = 0; 
+        this.dy = 0;
+        this.isMoving = false;
+      
+    }
     
-   }else{ // You won, so stop the game
+
+};
+
+
+function draw () {
+   
+   background(0, 67, 90);
+   
+   textSize(20);
+   fill(255, 255, 255);
+   text("Level " + robots.length, canvasW - 100, 20 );
+   text("Wait for all robots to start moving...",5, canvasH - 7);
+   
+   
+   gameRunning = false;
+   for(var i = 0; i < robots.length; i++){
+       gameRunning = gameRunning || robots[i].isMobile();
+   }
+   
+   
+    if(gameRunning){
+        
+        
+       
+       
+        for(var i = 0; i < robots.length; i++){
+            robots[i].draw();
+            robots[i].mouseAction();
+        }   
+        
+    }else if(robots.length != 8){  // Game is not running, all robots have stopped
+
        fill(92, 214, 88);
        strokeWeight(7);
-       rect( canvasW/2 - 80*2, canvasH/2 - 75, 310, 110);
+       rect(canvasW/2 - 80*2, canvasH/2 - 75, 310, 110);
        
        strokeWeight(1);
        fill(7, 117, 60);
@@ -199,30 +265,155 @@ draw = function() {
 	   fill(191, 0, 78);
 	   text(" CONTINUE ",canvasW/3.8 + 60, canvasH/1.1 + 2);
 	   
+       
 	   // Play Game Again
 	   if(mouseIsPressed   &&   
 	      mouseX > canvasW/2 - 73*2 && mouseX < canvasW/2 - 73*2 + 280  &&  
 		  mouseY > canvasH/1.6      && mouseY < canvasH/1.6 + 60)
-         stopGame = false;	   
+         restart();	   
 	   
 	   
 	   // Continue Game
 	   if(mouseIsPressed   &&   
 	      mouseX > canvasW/2 - 73*2 && mouseX < canvasW/2 - 73*2 + 280  &&  
 		  mouseY > canvasH/1.2      && mouseY < canvasH/1.2 + 60)
-         stopGame = false;
+         proceed();	
 		 
 		 
+        // Set limit for robots
+   }else{
+        fill(141, 188, 188);
+        rect(25, 25, canvasW - 50, canvasH - 50);
+        
+        
+        // Text
+        
+        var initX = 50;
+        var initY = 55;
+        fill(color(round(random(0, 256)), round(random(0, 256)), round(random(0, 256))));
+        text("CONGRATULATIONS!", initX , initY);
+        fill(95, 59, 123);
+        text("You've reached the end of the game!", initX , initY+40*1);
+        text("You can keep on playing in endless mode by clicking the button below.", initX , initY + 40*2);
+        text("However, be warned that the speed at which the robots move will be decreased", initX , initY + 40*3);
+        text("greatly as the number of robots on the screen increases.", initX , initY + 40*4);
+        
+        text("THANK YOU FOR PLAYING!", initX + 90, initY + 40*7);
+        
+        // CONTINUE button
+        fill(78, 75, 93);       
+        rect( canvasW/2 - 73*2, canvasH/1.2 , 280, 60);
+	    fill(27, 225, 210);       
+        rect( canvasW/2 - 73*2 + 2, canvasH/1.2 + 2, 280-4, 60-4);
+	    fill(27, 62, 178);
+        textSize(23);
+	    text(" CONTINUE ",canvasW/2.4, canvasH/1.11);
+        
+        
+        if(mouseIsPressed   &&   
+	      mouseX > canvasW/2 - 73*2 && mouseX < canvasW/2 - 73*2 + 280  &&  
+		  mouseY > canvasH/1.2      && mouseY < canvasH/1.2 + 60)
+          proceed();	
+   }
+       
+};
+
+
+
+
+
+Robot.prototype.move = function(dx, dy) {
+
+    this.x = this.x + dx;
+    this.y = this.y + dy;
+    
+};
+
+Robot.prototype.isMobile = function() {
+
+    return this.isMoving;
+    
+};
+
+Robot.prototype.energize = function() {
+
+    this.isMoving = true;
+    
+};
+
+Robot.prototype.getX = function() {
+
+    return this.x;
+    
+};
+
+Robot.prototype.getY = function() {
+
+    return this.y;
+    
+};
+
+
+
+Robot.prototype.getWidth = function() {
+
+    return this.w;
+    
+};
+
+Robot.prototype.getHeight = function() {
+
+    return this.h;
+    
+};
+
+Robot.prototype.changeSize = function(newSize) {
+
+    this.w = newSize;
+    this.h = newSize;
+    
+};
+
+/*
+var debugText = function(){
+    fill(0, 34, 255);
+    text("x: " + x, 100, 100);
+    text("y: " + y, 100, 120);
+    text("dx: " + dx, 100, 140);
+    text("dy: " + dy, 100, 160);
+    
+    //text(x < 0 && x + dx + w/2 > 400, 200, 200);
+    
+    text("Counter: " + timeCounter, 100, 200);
+    text(stopGame, 100, 220);
+    text(!stopGame, 100, 240);
+};
+*/
+
+var restart = function() {
+    //fill(144, 214, 90);
+    //rect(100,100,100,100);
+    
+    
+   for(var i = 0; i < robots.length; i++){
+       robots[i].energize();
+       robots[i].changeSize(100 - 5 * robots.length);
    }
    
-   
-   fill(78, 130, 230);
-   //drawShape(x, y, w, h);
-   drawRobot(x, y, w, h);
-   //debugText();
-   
-   
-       
+   gameRunning = true;
+
+};
+
+var proceed = function() {
+    var newX = round(random(50, canvasW - 110));
+    var newY = round(random(50, canvasH - 110));
+    var col = color(round(random(0, 256)), round(random(0, 256)), round(random(0, 256)));
+    
+    var newRobot = new Robot(newX, newY, 100, 100, col);
+    robots.push(newRobot);
+    
+    restart();
+
 };
 
 
